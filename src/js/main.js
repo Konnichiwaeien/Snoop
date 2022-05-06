292513972b03a4009095d476dbb76c0d955244bd
konnichiwaeien <konnichiwaeien@gmail.com>
2022-05-06 17:34:24 +0500

    before auth renaming

--- src/js/main.js
+++ src/js/main.js
@@ -16,8 +16,48 @@ const setDropdown = (dropdownSel) => {
   options.forEach((item) => {
     item.addEventListener('click', () => setValue(item.getAttribute('data-option')));
   });
-
-
 }
 
 setDropdown('.nav__dropdown');
+
+
+
+const setSlidePopup = (parentSel) => {
+  const parent = document.querySelector(parentSel);
+  const items = parent.querySelectorAll('.links__item');
+
+  items.forEach((item) => {
+    const popup = item.querySelector('.links__visits');
+    const btn = item.querySelector('.links__open');
+
+
+    btn.addEventListener('click', () => {
+      let isActive = popup.classList.contains('links__visits--active');
+
+      if (isActive) {
+        popup.classList.remove('links__visits--active');
+        btn.classList.remove('links__open--active');
+        popup.style.height = 0 + "px";
+      } else {
+        popup.classList.add('links__visits--active');
+        btn.classList.add('links__open--active');
+        popup.style.height = popup.querySelector(".links__inner").offsetHeight + "px";
+      }
+    });
+  });
+}
+
+setSlidePopup('.links')
+
+
+
+const clearSearchValue = () => {
+  const search = document.querySelector('.form__search');
+  const remove = document.querySelector('.form__remove');
+
+  remove.addEventListener('click', () => {
+    search.value = '';
+  });
+}
+
+clearSearchValue();
